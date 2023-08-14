16afdd0375369c493f8edc4f4ea505525275fb85
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-08-14 18:20:34 -0400

    add images, function to get dogObjects for hard mode

--- src/helpers/dogHelpers.js
+++ src/helpers/dogHelpers.js
@@ -1,4 +1,6 @@
-export const fetchDogData = async (setDogData) => {
+import { shuffle } from "./gameBoardHelpers";
+
+export async function fetchDogData(setDogData) {
   console.log("calling fetch dog image");
 
   const response = await fetch("https://dog.ceo/api/breeds/image/random/12");
@@ -11,8 +13,18 @@ export const fetchDogData = async (setDogData) => {
   }
 
   setDogData(dogObjects);
-};
+}
 
-const createDogObject = (url) => {
+function createDogObject(url) {
   return { image: url, clicked: false };
-};
+}
+
+export function getHardModeData(setDogData) {
+  const dogObjects = [];
+
+  for(let i = 1; i <= 12; i ++) {
+    dogObjects(createDogObject(`src/assets/sleeping_${i}.jpeg`))
+  }
+  shuffle(dogObjects);
+  setDogData(dogObjects);
+}
